6d7c869edd90960da622afee87fe993575fb1ffa
Norimarusuke <rei0208otsu@gmail.com>
2025-07-27 15:07:35 +0900

    hello

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("hello.html")
+
+@app.route("/jidou_hanbai")
+def jidou_hanbai():
+    return render_template("jidou_hanbai.html")
+
+@app.route("/convenience")
+def convenience():
+    return render_template("convenience.html")
+
+@app.route("/hikageno_basho")
+def hikageno_basho():
+    return render_template("hikageno_basho.html")
+
+@app.route("/top")
+def top():
+    return render_template("top.html")
+
+app.run(debug=True)
+
+
