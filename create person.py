5eabf2ee7f64604371f64869c2df1ca9582d2906
Norimarusuke <rei0208otsu@gmail.com>
2025-09-26 17:19:36 +0900

    new

---
+++ create person.py
@@ -0,0 +1,9 @@
+from database import Person
+Rei = Person(name = "おおつれい",age = 9, gender = "女")
+Rei.save()
+Rina = Person(name = "おばらりな",age = 9, gender = "女")
+Rina.save()
+Hinako = Person(name = "いまむらひなこ",age = 9, gender = "女")
+Hinako.save()
+Ichika = Person(name = "ささきいちか",age = 10, gender = "女")
+Ichika.save()
---
+++ database.py
@@ -0,0 +1,12 @@
+from peewee import *
+
+db = SqliteDatabase('s-c-b.db')
+
+class Person(Model):
+    name = CharField()
+    age = IntegerField()
+    gender = CharField()
+    class Meta:
+        database = db # This model uses the "people.db" database.
+
+db.create_tables([Person])
